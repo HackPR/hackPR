2ab02eaf18eed2daa272b9b66e5b857b5643af2c
David Bartolomei <david@dbartolomei.com>
2014-01-26 14:16:23 -0400

    meh

--- public/js/main.js
+++ public/js/main.js
@@ -2,6 +2,7 @@
 $(document).ready(function(){
   var height = $(window).height();
   $('.main_cover').css('min-height', height);
+
   // $('.content_section').css('min-height',height);
 
 
@@ -15,18 +16,28 @@ $(document).ready(function(){
 //fixing navbar
 $(document).ready(function(){
 	var device_height = $(window).height();
-	$(window).bind('scroll', function() {
-      if ($(window).scrollTop() > device_height) {
-        $('#navbar').addClass('fixed');
-        $('#content').css('margin-top', 80);
-        // console.log($('#content').height());
-       }
-       else {
-         $('#navbar').removeClass('fixed');
-        $('#content').css('margin-top', 0);
-          // console.log($('#content').height());
-       }
-    });
+	$(window).bind('scroll', function(){
+    if ($(window).scrollTop() > device_height){
+      $('#navbar').addClass('fixed');
+      $('#content').css('margin-top', 80);
+     }
+     else{
+      $('#navbar').removeClass('fixed');
+      $('#content').css('margin-top', 0);
+    }
+    $('.menu_offset').each(function(){
+      var scrollTop     = $(window).scrollTop();
+      var elementOffset = $(this).offset().top;
+      var distance      = (elementOffset - scrollTop);
+      $(this).removeClass('.active');
+      if(distance <= 40 && distance > 0){
+        var current = $(this).attr('id');
+        var nav = $('.'+current+'_nav').attr('href');
+        console.log(nav);
+        $(current+'_nav').addClass('.active');
+      }
+    })
+  });
 })
 
 //changing element
@@ -46,18 +57,6 @@ $(function() {
 });
 
 
-//change div on nav click
-// $('nav a').on('click', function() {
-//     var scrollAnchor = $(this).attr('href'),
-//         scrollPoint = $('#' + scrollAnchor).offset().top - 70;
-//     $('body,html').animate({
-//         scrollTop: scrollPoint
-//     }, 500);
-//     return false;
-
-// })
-
-
 $( 'nav a' ).on('click', function(event) {
     event.preventDefault();
     var target = "#" + $(this).data('target');
@@ -65,26 +64,6 @@ $( 'nav a' ).on('click', function(event) {
         scrollTop: $(target).offset().top
     }, 2000);
 });
-
-// change active class on nav elements
-// $(window).scroll(function() {
-//     var windscroll = $(window).scrollTop()+window.innerHeight;
-//     if (windscroll >= 100) {
-//         $('nav').addClass('fixed');
-//         $('.content_section').each(function(i) {
-//             if ($(this).position().top <= windscroll - 60) {
-//                 $('nav li.active').removeClass('active');
-//                 $('nav li').eq(i).addClass('active');
-//             }
-//         });
-//     } else {
-
-//         $('nav li.active').removeClass('active');
-//         $('nav li:first').addClass('active');
-//     }
-// }).scroll();
-
-
 
 
 //main submit form
