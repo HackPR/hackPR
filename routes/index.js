1b8799d555963d1b2908ddf111df04516e53a578
cesarcruz91 <cesarcruz91@gmail.com>
2014-01-27 23:25:49 -0400

    Added sponsorship docs

--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,14 @@
+var path = require('path');
+var fs = require('fs');
+
 exports.index = function(req, res, next){
   res.render('index');
+};
+
+exports.get_sponsorship = function(req, res, next) {
+  var file = '/public/sponsorship.pdf'
+  fs.readFile(file, function (err, buffer) {
+    res.contentType("application/pdf");
+    res.send(buffer);
+  });
 };
