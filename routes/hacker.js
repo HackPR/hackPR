33e9eb4e897a92d5ab46c7eec08157bf3f3dcab9
David Bartolomei <david@dbartolomei.com>
2014-02-21 18:54:34 -0400

    removing .env pkg

--- routes/hacker.js
+++ routes/hacker.js
@@ -1,5 +1,4 @@
 var db = require('./db');
-var dotenv = require('dotenv').load();
 var sendgrid = require('sendgrid')(process.env.SENDGRID_USERNAME, process.env.SENDGRID_PASSWORD);
 
 exports.collect = function (req, res, next) {
