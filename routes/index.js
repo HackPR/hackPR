07c739e7dcaa338b97550efdea0d3b4b5cf1f0db
David Bartolomei <david@dbartolomei.com>
2014-02-21 23:32:28 -0400

    adding robots.txt

--- routes/index.js
+++ routes/index.js
@@ -5,3 +5,9 @@ exports.index = function(req, res, next){
   res.render('index');
 };
 
+
+exports.robots = function(req, res, next){
+  res.set('Content-Type', 'text/plain');
+  var buffer = fs.readFileSync(__dirname + '/../robots.txt');
+  res.send(buffer);
+}
